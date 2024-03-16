bbd0bdaa1436ce9d282c72845127badaa2bb2644
Tomas Glabazna <tomas@glabazna.cz>
2024-03-16 21:06:04 +0100

    activity listener added

--- server/index.js
+++ server/index.js
@@ -24,10 +24,27 @@ const io = new Server(expressServer, {
 
 io.on('connection', socket => {
   console.log(`User ${socket.id} connected`)
+  // Upon connection - only to user
+  socket.emit('message', 'Welcome to chat app')
 
+  // Upon connection - to all others
+  socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} connected`)
+
+  // Litening for a message event
   socket.on('message', data => {
     console.log(data)
     io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
   })
+
+  // When user disconnects - to all others
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} disconnected`)
+  })
+
+  // Listen for activity
+  socket.on('activity', (name) => {
+    socket.broadcast.emit('activity', name)
+  })
 })
+
 
